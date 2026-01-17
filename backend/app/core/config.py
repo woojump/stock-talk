1d293612ac802d336e6caad3519438a91880cd2a
Wooju <woojukwon8@gmail.com>
2026-01-17 16:32:09 +0900

    fix: openai api key 주석 해제

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,8 +1,9 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
     PROJECT_NAME: str = "LLM-MCP Stock Platform"
-    # OPENAI_API_KEY: str  # 서버에서 사용할 AI 키
+    OPENAI_API_KEY: str  # 서버에서 사용할 AI 키
     MODEL_NAME: str = "gpt-4o-mini"
 
     KIWOOM_APP_KEY: str
@@ -10,8 +11,8 @@ class Settings(BaseSettings):
     # KIWOOM_ACCOUNT_NO: str
     DATA_PORTAL_KEY: str
 
-    
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
