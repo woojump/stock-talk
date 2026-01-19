10fc074813e17b17710f22607d57f13333422b6a
Wooju <woojukwon8@gmail.com>
2026-01-19 21:53:42 +0900

    feat: Firebase 설정

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:stock_talk/app/app.dart';
 import 'package:stock_talk/core/di/injection.dart';
 
+import 'core/config/firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await configureDependencies();
 
