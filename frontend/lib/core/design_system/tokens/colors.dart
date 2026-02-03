25b3974cc2a59bb190ebee451ba0b96f2f583988
Wooju <woojukwon8@gmail.com>
2026-02-03 23:01:27 +0900

    fix: 색상 토큰 수정 반영 일단 라이트 모드 색상만 정의

--- frontend/lib/core/design_system/tokens/colors.dart
+++ frontend/lib/core/design_system/tokens/colors.dart
@@ -8,8 +8,9 @@ class AppColors {
   // Common
   static const white = Color(0xFFFFFFFF);
   static const black = Color(0xFF222222);
+  static const transparent = Color(0x00000000);
 
-  // Gray scale
+  // Gray scale - Light
   static const gray800 = Color(0xFF222222);
   static const gray700 = Color(0xFF4A4E52);
   static const gray600 = Color(0xFF636970);
@@ -17,19 +18,15 @@ class AppColors {
   static const gray400 = Color(0xFFB5B9C2);
   static const gray300 = Color(0xFFE2E5E8);
   static const gray200 = Color(0xFFF3F3F3);
-  static const gray100 = Color(0xFFFAFAFA);
+  static const gray100 = Color(0xFFFCFCFC);
 
   // Blue
   static const blue500 = Color(0xFF0059CD);
-  static const blue400 = Color(0xFF0066FF); // Primary color
-  static const blue300 = Color(0xFF4BAEFE);
+  static const blue400 = Color(0xFF0077EA);
+  static const blue300 = Color(0xFF4BAEFE); // Primary color
   static const blue200 = Color(0xFFCBE8FE);
   static const blue100 = Color(0xFFEAEDF3);
 
   // Sub colors
   static const red = Color(0xFFFF604C);
-
-  // Semantic colors
-  static const textDefault = Color(0xFF222222);
-  static const borderDefault = Color(0xFFC2C2C2);
 }
